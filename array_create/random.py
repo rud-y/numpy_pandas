c7635ebe3d42647be8cbf431d4ef0272d7e7a3ef
rud-y <rudolf.zachar@gmail.com>
2026-06-16 12:16:32 +0100

    Accessing reshaped array elements

--- array_create/random.py
+++ array_create/random.py
@@ -7,17 +7,18 @@ rng = default_rng(12345)
 random_array = rng.random(3)
 # print(random_array)
 
-mean, stddev = 1, 0.5
+mean, stddev = 3, 0.5
 random_normal = rng.normal(mean, stddev, size=3)
-print(f"rantom_normal>> {random_normal}")
+print(f"random_normal --- {random_normal}")
 
-rng = default_rng(12345)
-age_range = rng.integers(1,101,10)
-print(age_range)
+range = default_rng(12345)
+age_range = range.choice(np.arange(30, 45), size=6, replace=False)
+print(f"age_range --- {age_range}")
 
-rng2 = np.random.default_rng(2022)
+rng2 = np.random.default_rng(5)
 random_arr_rng2 = rng2.random(9).reshape(3,3)
-print(random_arr_rng2)
+print("random range 2 --- ")
+# print(random_arr_rng2)
 
 arr_range = np.arange(10,101,10).reshape(5,2)
 arr_range
--- indexing_slicing/indexing-slicing.py
+++ indexing_slicing/indexing-slicing.py
@@ -1,5 +1,6 @@
 #%%
 import numpy as np
+from numpy.random import default_rng
 
 integer_arr = np.arange(12)
 print(f"array position -2: {integer_arr[-2]}")
@@ -10,11 +11,32 @@ print(integer_arr[::3])
 # [0, 3, 6, 9]
 
 new_array = integer_arr.reshape(3,4)
-# print(new_array)
-print(new_array[:, 1:])
+print(f"new_array reshape(3,4) ---- {new_array}")
+print(f"What??  ---{new_array[:, 1:]}")
 
 ages = np.array([5, 10, 15, 19, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80])
-ages[::3]
 
-# youth_ages = ages[10]
+youth_ages = ages[1:4]
+
+print(f"youth ages ---- {youth_ages}")
+
+# Accessing rows and columns or specific elements of an array
+rng = default_rng(12345)
+random_array = rng.random(9)
+
+array3matrix = random_array.reshape(3, 3)
+
+print(f"array3matrix ----- {array3matrix}")
+
+# Access only last row
+print(f"Last row --- {array3matrix[2, :]}")
+
+# Access middle column
+print(f"Middle column --- {array3matrix[:, 1]}")
+
+# Access only first element of the last column
+print(f"First element of the last column --- {array3matrix[0, 2]}")
+
+
+
 # %%
